77b4cf0ca55fede0bf7ffd20d7de334fcd117e6c
Dan Taeyoung <provolot@gmail.com>
2016-07-06 00:45:45 -0400

    api.py now gets results from mongotest

--- api/api.py
+++ api/api.py
@@ -1,22 +1,45 @@
 import requests
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+import settings
 
 
 app = Flask(__name__)
 
 
-@app.route('/get_tab')
+@app.route('/')
 def hello():
     return 'Hello, World'
 
 
 
 @app.route('/test')
-def data():
+def test():
     # here we want to get the value of user (i.e. ?user=some-value)
     url = request.args.get('url')
     print url
     return 'butts'
+
+
+@app.route('/get_tab')
+def get_tab():
+    # here we want to get the value of user (i.e. ?user=some-value)
+    secret = request.args.get('secret')
+    print secret  
+    if (secret == 'heyboi'):
+
+        client = MongoClient('mongodb://' + settings.mongouser + ':' + settings.mongopass + '@' + settings.mongourl)
+        db = client['manila_db']
+        collection_on_compose = db['urls']
+
+        one_url = collection_on_compose.find_one()
+
+        print one_url['url']
+        return one_url['url']
+
+    else:
+        return "butts"
+
 
 
 
--- api/mongotest.py
+++ api/mongotest.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import settings
 
-
 client = MongoClient('mongodb://' + settings.mongouser + ':' + settings.mongopass + '@' + settings.mongourl)
 db = client['manila_db']
 collection_on_compose = db['urls']
