061d46572854a47fe259e58904d5314d2e158c52
Dan Taeyoung <provolot@gmail.com>
2016-07-06 00:29:21 -0400

    making test in mongotest

---
+++ api/mongotest.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+import settings
+
+
+client = MongoClient('mongodb://' + settings.mongouser + ':' + settings.mongopass + '@' + settings.mongourl)
+db = client['manila_db']
+collection_on_compose = db['urls']
+
+print collection_on_compose.find_one()
+
