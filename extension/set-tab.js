e8e0eb26486740fd11b584048a61e72ef48676a2
melaniehoff <hoffmelanie@gmail.com>
2016-08-16 01:25:42 -0400

    set tab done

--- extension/set-tab.js
+++ extension/set-tab.js
@@ -2,14 +2,36 @@ console.log('set-tab.js');
 
 var currentUrl = window.location.href;
 
+var setTab;
+
+setTab = function(){
+	$.ajax({
+    url: "//vps.provolot.com/manila_api/set_tab?tabroom=surfclub&url="+currentUrl,
+    success: function(response) {
+        console.log('SUCCESS');
+    },
+    error: function(xhr) {
+        console.log('FAILURE');
+    }
+});
+
+    
+
+
+}
+
 
 
 chrome.runtime.onMessage.addListener(
   function(message) {
-    console.log('Button clicked!: ', message);
+
+  	console.log('Button clicked!: ', message);
+  	setTab();
+    
     console.log('________________________________________________');
     console.log('________________________________________________');
     console.log('THIS IS WILL BE SENT TO THE DATABASE!: ',currentUrl); 
+
 
 
 //window.open(currentUrl);
